c920d2387f7653dc0c30c1a54008dab5df0adb97
SanjanaReddy-2007 <aresanjanareddy@gmail.com>
2025-08-28 10:44:29 +0530

    Time: 0 ms (100%), Space: 9.7 MB (65.5%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -7,35 +7,34 @@ public:
     MyQueue() {
         
     }
-    
+    // 1.Copy elements of s1 into s2
+    // 2.Push remaining element into s1
+    // 3.Bring all the elements back to s1
     void push(int x) {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+
         while (!s2.empty()) {
             s1.push(s2.top());
             s2.pop();
         }
-        s1.push(x);
     }
     
     int pop() {
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        int val = s2.top();
-        s2.pop();
+        int val = s1.top();
+        s1.pop();
         return val;
     }
     
     int peek() {
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        return s2.top();
+        return s1.top();
     }
     
     bool empty() {
-        return (s1.empty() && s2.empty());
+        return (s1.empty());
     }
 };
 
