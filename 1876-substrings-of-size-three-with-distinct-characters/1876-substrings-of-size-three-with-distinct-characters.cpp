472bc4bbc4331d809c3d1daa74573808e9f00109
SanjanaReddy-2007 <aresanjanareddy@gmail.com>
2025-11-29 19:05:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (45.31%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        if (s.length() == 1 || s.length() == 2) {
-            return 0;
+           int n = s.size();
+        if (n < 3) return 0;
+
+        int freq[26] = {0};
+        int distinct = 0;
+        int ans = 0;
+
+        // Initialize the first window of size 3
+        for (int i = 0; i < 3; i++) {
+            if (freq[s[i] - 'a'] == 0) distinct++;
+            freq[s[i] - 'a']++;
         }
-        vector<string> res;
-        for (int i = 0; i < s.length()-2; i++) {
-            res.push_back(s.substr(i, 3));
+
+        if (distinct == 3) ans++;
+
+        // Slide the window
+        for (int i = 3; i < n; i++) {
+            char add = s[i];
+            char remove = s[i - 3];
+
+            // Add new character
+            if (freq[add - 'a'] == 0) distinct++;
+            freq[add - 'a']++;
+
+            // Remove old character
+            freq[remove - 'a']--;
+            if (freq[remove - 'a'] == 0) distinct--;
+
+            if (distinct == 3) ans++;
         }
-        int count = 0;
-        for (string str : res) {
-            if ((str[0] != str[1]) && (str[1] != str[2]) && (str[2] != str[0])) {
-                count++;
-            }
-        }
-        return count;
+
+        return ans;
     }
 };
