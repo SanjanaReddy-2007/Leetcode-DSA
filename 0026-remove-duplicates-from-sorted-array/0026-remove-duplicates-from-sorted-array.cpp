0e4fea5ac6c4ac3bc6ab449b39aaa548f0459607
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-06 22:12:13 +0530

    Time: 0 ms (100%), Space: 22.7 MB (49.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
 
         int j = 1;
         for (int i = 1; i < n; i++) {
@@ -12,6 +11,5 @@ public:
             }
         }
         return j;
-
     }
 };
