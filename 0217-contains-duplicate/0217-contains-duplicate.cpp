324fc3c15d5f8dad4b20dea313c630f208f01331
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-10 18:42:59 +0530

    Time: 44 ms (70.88%), Space: 90.7 MB (59.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> freq;
+        unordered_set<int> numSet;
 
         for (int n : nums) {
-            freq[n]++;
+            if (numSet.find(n) != numSet.end()) {
+                return true;
+            }
+            numSet.insert(n);
         }
-
-        for (auto it : freq) {
-            if (it.second > 1) return true;
-        }
-        return false;
+        
+        return false;        
     }
 };
