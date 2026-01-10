7b877d8401b277e823a852eae344ef320a817a4f
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-10 20:36:06 +0530

    Time: 0 ms (100%), Space: 31.4 MB (44.28%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>&arr, int target) {
+        int st = 0, end = arr.size()-1;
+        while (st <= end) {
+            int mid = st + (end-st)/2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } 
+            else if (arr[mid] > target) {
+                end = mid-1;
+            } else {
+                st = mid+1;
+            }
+        }
+        return -1;
+    }
+};
