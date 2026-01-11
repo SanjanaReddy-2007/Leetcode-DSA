4b9669ddd58d66d71df31d98baebdb656e324928
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-11 09:46:32 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,20 +6,19 @@ public:
         while (st <= end) {
             int mid = st + (end-st)/2;
 
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[st] <= nums[mid]) { // Left sorted
-                if (nums[st] <= target && target <= nums[mid]) { // Target on left 
-                    end = mid - 1;
+            if (nums[mid] == target) return mid;
+
+            if (nums[st] <= nums[mid]) { // If first half is sorted
+                if (nums[st] <= target && target < nums[mid]) {
+                    end = mid-1;
                 } else {
-                    st = mid + 1;
+                    st = mid+1;
                 }
-            } else { // Right Sorted
-                if (nums[mid] <= target && target <= nums[end]) { // Target on right
-                    st = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    st = mid+1;
                 } else {
-                    end = mid - 1;
+                    end = mid-1;
                 }
             }
         }
