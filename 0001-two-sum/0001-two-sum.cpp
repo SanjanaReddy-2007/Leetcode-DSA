e489d3ed2eed1bd9105252a4986b1c701020b3b1
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-11 09:53:53 +0530

    Time: 0 ms (100%), Space: 14.8 MB (52.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans;
+        unordered_map<int,int> mp;
 
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                sum = nums[i] + nums[j];
-                if (sum == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            if (mp.count(need)) {
+                return {mp[need], i};
             }
+            mp[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };
