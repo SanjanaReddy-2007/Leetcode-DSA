b8aee515913bf8d1440e375bcd39c64eec9970c1
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-01-12 07:11:36 +0530

    Time: 0 ms (100%), Space: 13.8 MB (26.76%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -3,9 +3,7 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int ans = 0;
         for (int i = 1; i < points.size(); i++) {
-            int dx = abs(points[i][0] - points[i-1][0]);
-            int dy = abs(points[i][1] - points[i-1][1]);
-            ans += max(dx, dy);
+            ans += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1]));
         }
         return ans;
     }
