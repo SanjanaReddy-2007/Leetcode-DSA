958871e264294332fdfa4fd4bb987c317b499954
Sanjana Reddy Are <aresanjanareddy@gmail.com>
2026-02-19 07:05:46 +0530

    Time: 3 ms (67.83%), Space: 13 MB (82.66%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int n = s.size();
+        int prev = 0, curr = 1, count = 0;
+        for (int i = 1; i < n; i++) {
+            if (s[i] == s[i-1]) curr++;
+            else {
+                count += min(curr, prev);
+                prev = exchange(curr, 1);
+            }
+        }
+        count += min(curr,prev);
+        return count;
+    }
+};
